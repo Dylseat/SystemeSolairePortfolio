392f550ad8f856862aa342230916d08527dff9a8
Dylseat <dyl0301@protonmail.com>
2026-05-10 14:10:54 +0200

    add planete compétences

--- frontend/src/components/SolarScene.tsx
+++ frontend/src/components/SolarScene.tsx
@@ -1,5 +1,48 @@
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls, Stars } from '@react-three/drei'
+import { OrbitControls, Stars, Text } from '@react-three/drei'
+
+function Sun() {
+  return (
+    <mesh>
+      <sphereGeometry args={[1, 32, 32]} />
+      <meshStandardMaterial
+        color="orange"
+        emissive="orange"
+        emissiveIntensity={1.5}
+      />
+    </mesh>
+  )
+}
+
+function Orbit({ radius }: { radius: number }) {
+  return (
+    <mesh rotation={[Math.PI / 2, 0, 0]}>
+      <torusGeometry args={[radius, 0.01, 16, 100]} />
+      <meshBasicMaterial color="#334155" />
+    </mesh>
+  )
+}
+
+function Planet() {
+  return (
+    <group position={[3, 0, 0]}>
+      <mesh>
+        <sphereGeometry args={[0.35, 32, 32]} />
+        <meshStandardMaterial color="#3b82f6" />
+      </mesh>
+
+      <Text
+        position={[0, 0.65, 0]}
+        fontSize={0.22}
+        color="white"
+        anchorX="center"
+        anchorY="middle"
+      >
+        Compétences
+      </Text>
+    </group>
+  )
+}
 
 function SolarScene() {
   return (
@@ -18,14 +61,9 @@ function SolarScene() {
         speed={1}
       />
 
-      <mesh>
-        <sphereGeometry args={[1, 32, 32]} />
-        <meshStandardMaterial
-          color="orange"
-          emissive="orange"
-          emissiveIntensity={1.5}
-        />
-      </mesh>
+      <Sun />
+      <Orbit radius={3} />
+      <Planet />
 
       <OrbitControls />
     </Canvas>
